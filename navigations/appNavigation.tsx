fd236d5b34f348c85eaf77b37578d79b95332434
manasvi runwal <manasvirunwal@gmail.com>
2021-09-01 11:49:10 +0530

    upload

---
+++ navigations/appNavigation.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from '../screens/homeScreen';
+import { DetailScreen } from '../screens/detailsScreen';
+
+const Stack = createStackNavigator();
+
+const Navigation = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} options={{
+                    title: 'INFO',
+                    headerStyle: {
+                        backgroundColor: 'blue'
+                    },
+                    headerTintColor: 'white',
+                    headerTitleStyle: {
+                        fontWeight: 'bold'
+                    }
+                }} />
+                <Stack.Screen
+                    name="Details"
+                    component={DetailScreen}
+                    options={{
+                        title: 'Details',
+                        headerStyle: {
+                            backgroundColor: 'blue'
+                        
+                        },
+                        headerTintColor: 'white',
+                        headerTitleStyle: {
+                            fontWeight: 'bold'
+                        }
+                    }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+export default Navigation
